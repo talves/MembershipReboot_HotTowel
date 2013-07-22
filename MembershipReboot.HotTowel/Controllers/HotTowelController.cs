598574467ac50c8d58e2cf0fc1b17b7e71ca67fa
talves <tony@3alves.com>
2013-07-22 11:59:50 -0700

    Add Base HotTowel Template to project

---
+++ MembershipReboot.HotTowel/Controllers/HotTowelController.cs
@@ -0,0 +1,16 @@
+using System.Web.Mvc;
+
+namespace MembershipReboot.HotTowel.Controllers
+{
+    public class HotTowelController : Controller
+    {
+        //
+        // GET: /HotTowel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MembershipReboot.HotTowel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MembershipReboot.HotTowel.MvcApplication" Language="C#" %>
